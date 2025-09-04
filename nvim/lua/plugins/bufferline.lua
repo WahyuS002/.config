fb2076df660cbce25c6e13cdcff188df600df2d5
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2025-09-04 21:46:16 +0700

    feat(nvim): always open new buffers to the right of the current one

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -70,10 +70,7 @@ return {
                     separator_style = 'thin', -- | "thick" | "thin" | { 'any', 'any' },
                     enforce_regular_tabs = true,
                     always_show_bufferline = true,
-                    -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
-                    --   -- add custom logic
-                    --   return buffer_a.modified > buffer_b.modified
-                    -- end
+                    sort_by = 'insert_at_end',
                     offsets = {
                         {
                             filetype = 'NvimTree',
